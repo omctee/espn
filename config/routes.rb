ac5cda466579a2998afa2f7c1017462f2f94eabc
None None <none@none.com>
2018-11-16 14:46:28 -0500

    Add User y Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :messages #creates all routes for app
   root 'messages#index' #initial page of app responds to message controller, index action
 
