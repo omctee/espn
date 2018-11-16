72ea8d2e144295fda6502b8150cfa0e63347c568
None None <none@none.com>
2018-11-16 13:34:05 -0500

    Add Index y Show pages/actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,43 @@
 class MessagesController < ApplicationController
+
+
   def index
+    @messages = Message.all.order("created_at DESC")
   end
+
+  def show
+    @message = Message.find(params[:id])
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :description)
+  end
+
+  def find_message
+    @message = Item.find(params[:id])
+  end
+
+
 end
